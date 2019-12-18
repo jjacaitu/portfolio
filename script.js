3dd446d6e0d2941e867940e819a0b3fccbfd5466
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-12-17 22:01:22 -0500

    added icons to the switch

--- script.js
+++ script.js
@@ -3,26 +3,25 @@ $(function () {
     // Get the toggleSwitch element ("checkbox")
     const toggleSwitch = document.querySelector('input[type="checkbox"].switch');
     
-    // Set the checkbox to false ("not checked")
-    toggleSwitch.checked = false;
+    // Set the checkbox to true ("checked")
+    toggleSwitch.checked = true;
     
     // Function to run when the checkbox changes state
     function switchTheme(e) {
         const images = document.querySelectorAll(".skillsMain img");
         
-        // If it is checked changed the theme to light and change the 3 icons that are images to the correct color
+        // If it is checked changed the theme to light and change the 2 icons that are images to the correct color
         if (e.target.checked) {
             document.documentElement.setAttribute('data-theme', 'light');
             images[0].src = "./assets/icon-firebase - light.svg";
-            images[1].src = "./assets/icon-w3c - light.svg";
-            images[2].src = "./assets/icon-tablet-phone-and-browser - light.svg";
+            images[1].src = "./assets/icon-tablet-phone-and-browser - light.svg";
         }
-        // If it is checked changed the theme to dark and change the 3 icons that are images to the correct color
+        // If it is checked changed the theme to dark and change the 2 icons that are images to the correct color
         else {
             document.documentElement.setAttribute('data-theme', 'dark');
             images[0].src = "./assets/icon-firebase.svg";
-            images[1].src = "./assets/icon-w3c.svg";
-            images[2].src = "./assets/icon-tablet-phone-and-browser.svg";
+            images[1].src = "./assets/icon-tablet-phone-and-browser.svg";
+            
         }
     }
     
